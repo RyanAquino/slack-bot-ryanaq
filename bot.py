2755eea96316a1dd2476a93f2d54c2f7c61265e6
Ryan Aquino (IS-PH) <ryan_aquino@trendmicro.com>
2018-08-06 22:31:55 +0800

    added comments and format text

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
+'''Slackbot program that post top 10 trending tweets on twitter'''
+
 import time
 import re
+import config
 from tweepy import API
 from tweepy import OAuthHandler
 from operator import itemgetter
-import config
 from slackclient import SlackClient
-
-starterbot_id = None
-
-RTM_READ_DELAY = 1
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+from threading import Timer
 
 config.slack_client = SlackClient(config.slack_client)
+
+starterbot_id = None
+RTM_READ_DELAY = 1
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+tryInput = 'post trending'
 
 def parse_bot_commands(slack_events):
     for event in slack_events:
@@ -26,12 +29,12 @@ def parse_direct_mention(message_text):
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def handle_command(command, channel):
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
-    response = None
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    elif command.startswith("do post"):
-        response = twitter_trend()
+    default_response = "Not sure what you mean. Try *{}*.".format(tryInput)
+
+    if command.startswith("post"):
+        response ="Top 10 trending tweets: \n" + twitter_trend()
+    else:
+        response = default_response
 
     config.slack_client.api_call(
         "chat.postMessage",
@@ -39,9 +42,7 @@ def handle_command(command, channel):
         text=response or default_response
     )
 
-from threading import Timer
-
-def sendResponse(channel='assignment1'):
+def sendResponse():
     config.slack_client.api_call(
         "chat.postMessage",
         channel='assignment1',
@@ -61,6 +62,8 @@ def twitter_trend():
     temp_trends = trends['trends']
 
     for trnd in temp_trends:
+        sortedTweets.append(trnd)
+
     sortedTweets = sorted(sortedTweets, key=itemgetter('tweet_volume'),reverse=True)
 
     ctr = 1
@@ -68,17 +71,17 @@ def twitter_trend():
 
     for test in sortedTweets:
         if ctr <= 10:
-            top10.append(test['name'])
+            top10.append(str(ctr) + '. ' + test['name'])
             ctr +=1
 
-    trending10 = ', \n'.join(top10)
-
+    trending10 = ' \n'.join(top10)
     return trending10
 
 if __name__ == "__main__":
     if config.slack_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
         sendResponse()
+
         starterbot_id = config.slack_client.api_call("auth.test")["user_id"]
         while True:
             command, channel = parse_bot_commands(config.slack_client.rtm_read())
@@ -86,6 +89,6 @@ if __name__ == "__main__":
                 handle_command(command, channel)
             time.sleep(RTM_READ_DELAY)
     else:
-        print("Connection failed. Exception traceback printed above.")
+        print("Connection failed! Try again")
 
 
